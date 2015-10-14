d0e9e1576fb674b4da88aef97ba493cffdeea82a
Niklas Keller <me@kelunik.com>
2015-10-14 15:11:42 +0200

    Add very verbose comments

--- aerys.php
+++ aerys.php
@@ -6,11 +6,17 @@ use Kelunik\Demo\Chat;
 use function Aerys\root;
 use function Aerys\websocket;
 
+// route /ws to the websocket endpoint
+// you can add more routes to this router
 $router = (new Router())
     ->route("GET", "ws", websocket(new Chat));
 
+// add document root
 $root = root(__DIR__ . "/public");
 
+// create virtual host localhost:1337
+// requests will first be routed, if no route matches, the server tries to find a file in the document root
+// you can add more responders or even multiple document roots to a single host
 (new Host)
     ->name("localhost")
     ->expose("*", 1337)
--- src/Chat.php
+++ src/Chat.php
@@ -14,12 +14,15 @@ class Chat implements Websocket {
     private $ips;
 
     public function onStart(Websocket\Endpoint $endpoint) {
+        // On server startup we get an instance of Aerys\Websocket\Endpoint, so we can send messages to clients.
         $this->endpoint = $endpoint;
         $this->connections = [];
         $this->ips = [];
     }
 
     public function onHandshake(Request $request, Response $response) {
+        // During handshakes, you should always check the origin header, otherwise any site will
+        // be able to connect to your endpoint. Websockets are not restricted by the same-origin-policy!
         $origin = $request->getHeader("origin");
 
         if ($origin !== "http://localhost:1337") {
@@ -29,18 +32,26 @@ class Chat implements Websocket {
             return null;
         }
 
+        // returned values will be passed to onOpen, that way you can pass cookie values or the whole request object.
         return $request->getConnectionInfo()["client_addr"];
     }
 
     public function onOpen(int $clientId, $handshakeData) {
+        // We keep one map for all connected clients.
         $this->connections[$clientId] = $handshakeData;
+        // And another one for multiple clients with the same IP.
         $this->ips[$handshakeData][$clientId] = true;
     }
 
     public function onData(int $clientId, Websocket\Message $msg) {
-        $body = yield $msg; // buffer all data
+        // yielding $msg buffers the complete payload into a single string. For very large payloads, you may want to
+        // stream those instead of buffering them.
+        $body = yield $msg;
+
+        // We use the IP as name for this simple chat app.
         $ip = $this->connections[$clientId];
 
+        // If someone mentions an IP, we send the message only to clients with that IP and the sender itself.
         if (preg_match("~@(\\d+\\.\\d+\\.\\d+\\.\\d+)\\b~", $body, $match)) {
             list($all, $receiver) = $match;
 
@@ -50,6 +61,8 @@ class Chat implements Websocket {
             if (!empty($clients)) {
                 $this->endpoint->broadcast($payload, $clients);
             }
+
+            $this->endpoint->send($clientId, $payload);
         } else {
             $payload = $ip . ": " . $body;
             $this->endpoint->broadcast($payload);
@@ -57,6 +70,7 @@ class Chat implements Websocket {
     }
 
     public function onClose(int $clientId, int $code, string $reason) {
+        // Always clean up data when clients disconnect, otherwise we'll leak memory.
         $ip = $this->connections[$clientId];
 
         unset($this->connections[$clientId]);
@@ -68,7 +82,6 @@ class Chat implements Websocket {
     }
 
     public function onStop() {
-        $this->connections = [];
-        $this->ips = [];
+        // intentionally left blank
     }
 }
